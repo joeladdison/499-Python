4c488dee49b6853f051d98b01a6c54753f4fbd95
Joel Addison <joel@addison.net.au>
2013-11-12 15:43:44 +1000

    Add Licence

--- serv499.py
+++ serv499.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
+
+# serv499 (CSSE2310 Assignment 4, 2013)
+#
+# Copyright (c) 2013, Joel Addison (jea)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 from __future__ import print_function
 import sys
 import socket
@@ -230,6 +257,7 @@ def get_bids(game):
                 bid = get_client_input_timeout(game, p, timeout=60)
                 if not game.running:
                     return
+                print("Bid: '%s', Game: '%s'" % (bid, game.name), file=sys.stderr)
                 bid_result = valid_bid(current_bid, bid)
             if bid_result == BID_PASS:
                 send_message_to_players(game, "%s passes" % p.name,
